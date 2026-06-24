83b8bb4154d47d8617f4cd27be07acfe60488cdb
DIGVIJAY TRIPATHY <tripathydigvijay7377@gmail.com>
2026-06-24 22:26:26 +0530

    add dependecies and dev-dependencies and structured the directory

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,11 @@ import './App.css'
 
 function App() {
   return (
-    <>
-      
-    </>
+    <div className="min-h-screen bg-slate-900 flex items-center justify-center">
+      <h1 className="text-4xl font-bold text-indigo-400">
+        VideoTube 🎬
+      </h1>
+    </div>
   )
 }
 
